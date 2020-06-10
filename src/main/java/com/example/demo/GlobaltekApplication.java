fc50544e7db1e2a77c218ee697d4475122b5afa0
yocuervoc <yocuervoc@unal.edu.co>
2020-06-10 12:01:30 -0500

    first commit

---
+++ src/main/java/com/example/demo/GlobaltekApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobaltekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobaltekApplication.class, args);
+	}
+
+}
