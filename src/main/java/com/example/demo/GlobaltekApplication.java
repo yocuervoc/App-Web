49dfd82fbb5f081bf9f1eb545410cad96d37999f
yocuervoc <yocuervoc@unal.edu.co>
2020-06-10 19:21:56 -0500

    user BCrypt

--- src/main/java/com/example/demo/GlobaltekApplication.java
+++ src/main/java/com/example/demo/GlobaltekApplication.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class GlobaltekApplication {
 
 	public static void main(String[] args) {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
+        System.out.println(bCryptPasswordEncoder.encode("1234"));
 		SpringApplication.run(GlobaltekApplication.class, args);
 	}
 
